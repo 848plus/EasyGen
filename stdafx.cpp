84c93d97aa58a0a186c80e380de7e648cc132db6
848plus <bytedike@gmail.com>
2020-10-01 04:35:54 +0800

    Bumping up copyright year

---
+++ stdafx.cpp
@@ -0,0 +1,10 @@
+//
+// Copyright (c) EasyCheng. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+// stdafx.cpp : source file that includes just the standard includes
+// cn.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+//
+
+#include "stdafx.h"
